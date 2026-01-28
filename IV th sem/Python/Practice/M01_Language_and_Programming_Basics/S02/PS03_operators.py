404943cb563c9069473729c6de63f837df61f063
Nikethchary <nikethchary.07@gmail.com>
2026-01-28 11:28:49 +0530

    statements

---
+++ IV th sem/Python/Practice/M01_Language_and_Programming_Basics/S02/PS03_operators.py
@@ -0,0 +1,15 @@
+x = int(input("Enter a value: "))
+print(x << 2)
+print(x >> 2)
+print(~x)
+
+#membership operator
+s = input()
+print("on" in s)
+
+#identity operator ==> is, is not
+x = 20
+y = 30
+z = 20
+print(x is y)
+print(x is z)
---
+++ IV th sem/Python/Practice/M01_Language_and_Programming_Basics/S02/PS04_conditional_statements.py
@@ -0,0 +1,14 @@
+'''
+num = int(input())
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")    
+'''
+num = int(input())
+if num > 0:
+    print("Positive")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative")
