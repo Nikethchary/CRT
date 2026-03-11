0b969b3b6a3cc4dc5a9234f122c0defc2c18f192
Nikethchary <nikethchary.07@gmail.com>
2026-03-11 11:03:36 +0530

    Practice

--- IV-SEM/Python/Practice/M03_Core_Concepts/S01/PS02_Python_collections.py
+++ IV-SEM/Python/Practice/M03_Core_Concepts/S01/PS02_Python_collections.py
@@ -4,6 +4,33 @@ s = 0
 for ele in nums:
     s += ele
     res.append(s)
-print(res)
-'''
+print(res)'''
 
+'''t = (1,2,3,45,50)
+t2 = (2,3,547,8)
+print(t,t2)
+
+t = (1,2,3,45,50)
+print(t[1:])
+print(t[0:3])
+
+t = (1,2,3,45,50)
+del t
+print(t)'''
+
+d = {"name":"Niketh", "age":20}
+print(d.get('name'))
+print(d.keys())
+print(d.values())
+
+d = {"name":"Niketh", "age":20}
+d['phn'] = 1234567890
+print(d)
+d['name'] = "Niketh chary"
+print(d)
+
+d = {"name":"Niketh", "age":20}
+del d['age']
+print(d.popitem())
+d.clear()
+print(d)
